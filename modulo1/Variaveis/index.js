2cbe4ced2b5ae22e6ded41d7f06cc2199a04947d
Miguel Santana Chamadouro de Paula <mg.chamadouro@gmail.com>
2022-05-09 16:36:44 -0300

    variaveis

---
+++ modulo1/Variaveis/index.js
@@ -0,0 +1,63 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+// vai sair o valor igual a 15, pois após o primeiro conso.log a let sofreu alteração no seu valor,
+// e foi colocalo um novo console logo após alteração de valor, por tanto vai aparecer 15, ou 10, 5.
+
+
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+// console.log(a, b, c)
+//acredito que 10,10,10
+
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+// let cargaHorária = prompt("Quantas horas você trabalha por dia?");
+// let salário = prompt("Quanto você recebe por dia?");
+
+
+
+//EXERCICIO DE ESCRITA DE CODIGO
+
+ // let = null
+ // let = null
+// C typeof(let);
+// D foi impressso indefinido, pois não a nenhum valor atribuido as variaves.
+//   let nome =  typeof prompt("Qual seu nome?");
+//   let idade = typeof prompt("Qual sua idade?"); 
+  //console.log(typeof nome + typeof idade); // F String
+  
+ //G 
+//  console.log("Olá, meu nome é", nome, "e tenho anos", idade); // para executar esse eu tirei typeof 
+ //das variaveis, depois voltei ao normal ao rodar e funcionar.
+
+ //2
+//  let perguntas = prompt("Esta usando sapatos?");
+//  console.log("Está usando sapatos",perguntas);
+// perguntas = prompt("Esta usando camisa?");
+//  console.log("Esta usando camisa", perguntas);
+// perguntas = prompt("Já almoçou?");
+//  console.log("Já almoçou", perguntas);
+
+//3
+
+// let a = 10;
+// let b = 25;
+// let c = null
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log("Novo valor de a é", a);
+// console.log("Novo valor de b é", b);
